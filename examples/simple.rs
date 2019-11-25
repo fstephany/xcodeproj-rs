95592af016f33f2982472e1f3282e747614b2c39
Francois Stephany <francois@tamere.eu>
2019-11-25 17:32:03 +0100

    Simple Example showing Targets and groups of a given pbxproj

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,10 +1,35 @@
 use plist;
-use xcodeproj::objects::PBXProj;
+use xcodeproj::objects::{PBXObject, PBXProj};
 
 fn main() {
-    let project: PBXProj = plist::from_file("./examples/data/Recordings.xcodeproj/project.pbxproj")
+    let proj: PBXProj = plist::from_file("./examples/data/Recordings.xcodeproj/project.pbxproj")
         .expect("failed to read project.pbxproj");
 
-    println!("{:?}", project);
+    println!("Targets:");
+    println!("========");
+    let main_project = proj.root_project().expect("could not get main project");
+    let targets = main_project
+        .targets
+        .iter()
+        .filter_map(|target_ref| match proj.get(target_ref) {
+            Some(PBXObject::PBXNativeTarget(target)) => Some(target),
+            Some(_) => None,
+            None => None,
+        });
+
+    for target in targets {
+        println!(
+            "- {}, {} - {}",
+            target.product_name, target.name, target.product_type
+        );
+    }
+
+    // Top Level group is C9EB56B41EAE33C1000EC5F4
+    println!("Groups:");
+    println!("=======");
+    for group in proj.groups().iter() {
+        println!("- {}", group);
+    }
+
     println!("Done");
 }
--- src/objects.rs
+++ src/objects.rs
@@ -1,16 +1,53 @@
 use serde::Deserialize;
 use std::collections::HashMap;
-
+use std::fmt::Display;
 /// Represents a .pbxproj file
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct PBXProj {
-    archive_version: usize,
-    object_version: usize,
+    pub archive_version: usize,
+    pub object_version: usize,
 
-    /// Root object references
+    /// Root object reference
     root_object: String,
     objects: HashMap<String, PBXObject>,
+}
+
+impl PBXProj {
+    pub fn groups(&self) -> Vec<&PBXGroup> {
+        self.objects
+            .iter()
+            .filter_map(|(_id, object)| match object {
+                PBXObject::PBXGroup(group) => Some(group),
+                _ => None,
+            })
+            .collect()
+    }
+
+    /// All the targets contained in the pbxproj file.
+    /// Beware that they might not be the ones from the root_project.
+    pub fn targets(&self) -> Vec<&PBXNativeTarget> {
+        self.objects
+            .iter()
+            .filter_map(|(_id, object)| match object {
+                PBXObject::PBXNativeTarget(target) => Some(target),
+                _ => None,
+            })
+            .collect()
+    }
+
+    pub fn get(&self, id: &str) -> Option<&PBXObject> {
+        self.objects.get(id)
+    }
+
+    pub fn root_project(&self) -> Option<&PBXProject> {
+        self.objects
+            .get(&self.root_object)
+            .and_then(|candidate| match candidate {
+                PBXObject::PBXProject(project) => Some(project),
+                _ => None,
+            })
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -19,10 +56,10 @@ pub struct PBXProj {
 pub enum PBXObject {
     #[serde(rename_all = "camelCase")]
     PBXBuildFile {
-        file_ref: String
+        file_ref: String,
     },
     PBXContainerItemProxy,
-    
+
     PBXBuildPhase,
     PBXAppleScriptBuildPhase,
     PBXCopyFilesBuildPhase,
@@ -42,25 +79,65 @@ pub enum PBXObject {
     PBXLegacyTarget,
     PBXNativeTarget(PBXNativeTarget),
 
-    PBXProject,
+    PBXProject(PBXProject),
     PBXTargetDependency,
     XCBuildConfiguration,
     XCConfigurationList,
 }
 
-// children 	List 	A list of element reference 	The objects are a reference to a PBXFileElement element.
-// name 	String 	The filename. 	
-// sourceTree 	String 	See the PBXSourceTree enumeration.
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PBXProject {
+    /// Reference to a XCConfigurationList element.
+    pub build_configuration_list: String,
+    /// Representation of the XcodeCompatibilityVersion.
+    pub compatibility_version: String,
+    /// The region of development.
+    pub development_region: String,
+    /// Whether file encodings have been scanned (0 or 1)
+    pub has_scanned_for_encodings: usize,
+    /// The known regions for localized files.
+    pub known_regions: Vec<String>,
+    /// Reference to a PBXGroup element.
+    pub main_group: String,
+    /// Reference to a PBXGroup element.
+    pub product_ref_group: String,
+    /// The relative path of the project.
+    pub project_dir_path: String,
+    /// String 	The relative root path of the project.
+    pub project_root: String,
+    /// References to PBXTargets.    
+    pub targets: Vec<String>,
+    // Array of map 	Each map in the array contains two keys: ProductGroup and ProjectRef.
+    //pub project_references: String,
+}
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct PBXGroup {
-    name: Option<String>,
-    path: Option<String>,
+    pub name: Option<String>,
+    pub path: Option<String>,
 
     /// References to PBXFileElement elements
-    children: Vec<String>,
-    source_tree: PBXSourceTree,
+    pub children: Vec<String>,
+    pub source_tree: PBXSourceTree,
+}
+
+impl Display for PBXGroup {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "PBXGroup {{ ")?;
+
+        if let Some(name) = &self.name {
+            write!(f, "name: {} ", name)?;
+        }
+
+        if let Some(path) = &self.path {
+            write!(f, "path: {} ", path)?;
+        }
+
+        write!(f, "sourceTree: {:?} ", self.source_tree)?;
+        write!(f, "}}")
+    }
 }
 
 #[derive(Deserialize, Debug)]
@@ -90,9 +167,12 @@ pub enum PBXSourceTree {
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "camelCase")]
 pub struct PBXNativeTarget {
-    name: String,
-    product_name: String,
-    product_install_path: Option<String>,
-    /// reference
-    build_configuration_list: String, 
+    pub name: String,
+    pub product_name: String,
+    pub product_install_path: Option<String>,
+
+    /// Examples:
+    /// - "com.apple.product-type.application"
+    /// - "com.apple.product-type.bundle.unit-test"
+    pub product_type: String,
 }
